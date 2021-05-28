526d3e4e3c1d8cc231c7507b60f546803fa697e5
Michael Kayvon Avishan <michaelkavishan@gmail.com>
2021-05-27 21:47:56 -0400

    adding rds instance

--- main.tf
+++ main.tf
@@ -17,11 +17,22 @@ resource "aws_s3_bucket" "stock_data_bucket" {
 resource "aws_s3_bucket_object" "stock_data_all" {
   bucket = aws_s3_bucket.stock_data_bucket.id
   key    = "all_stocks_5yr.csv"
-  source = "C:\Users\micha\OneDrive\Documents\AWS\ToS3\all_stocks_5yr.csv"
+  source = "C:/Users/micha/OneDrive/Documents/AWS/ToS3/all_stocks_5yr.csv"
 }
 
 resource "aws_s3_bucket_object" "stock_data_aapl" {
   bucket = aws_s3_bucket.stock_data_bucket.id
   key    = "AAPL_data.csv"
-  source = "C:\Users\micha\OneDrive\Documents\AWS\ToS3\AAPL_data.csv"
+  source = "C:/Users/micha/OneDrive/Documents/AWS/ToS3/AAPL_data.csv"
+}
+
+resource "aws_db_instance" "rds_instance" {
+  allocated_storage    = 5
+  engine               = "postgres"
+  engine_version       = "12.5"
+  instance_class       = "db.t3.micro"
+  name                 = "stock_market"
+  username             = "foo"
+  password             = "foobar123"
+  skip_final_snapshot  = true
 }
